887fb4abb428415e6ad72644bc1f189e51a31dcd
N R <mnpuzzle@gmail.com>
2018-12-11 13:10:29 +0100

    REACT working with signup, login, Home and Forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './Components/Map/Map'
+import React, { Component } from "react";
+import "./App.css";
+import Signup from "./Components/auth/Signup";
+import Login from "./Components/auth/Login";
+import AuthService from "./Components/auth/AuthService";
+import { Route, Link } from "react-router-dom";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: null
+    };
+
+    this.authService = new AuthService();
+
+    this.fetchUser();
+  }
+
+  fetchUser = () => {
+    this.authService
+      .loggedin()
+      .then(user => this.setState({ ...this.state, user }));
+  };
+
+  getUser = user => {
+    this.setState({ ...this.state, user });
+  };
+
+  logout = () => {
+    this.authService
+      .logout()
+      .then(() => this.setState({ ...this.state, user: null }));
+  };
+
   render() {
+    const welcome = this.state.user ? (
+      <div>
+        <p>Hola {this.state.user.username}</p>
+        <button onClick={this.logout}>Logout</button>
+      </div>
+    ) : (
+      <div>
+        <p>No user</p>
+        <Link to="/">Home</Link> - <Link to="/signup">Signup</Link> -{" "}
+        <Link to="/login">Login</Link>
+      </div>
+    );
+
     return (
       <div className="App">
-       <Map />
+        {welcome}
+       
+        <Route
+          path="/signup"
+          render={() => <Signup getUser={this.getUser} />}
+        />
+        <Route path="/login" render={() => <Login getUser={this.getUser} />} />
       </div>
     );
   }
